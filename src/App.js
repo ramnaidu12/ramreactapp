5ddaf75668d5aa1f0a6a5f606a9f48b70e5b7f18
ramnaidu12 <ramnaidudoddi415@gmail.com>
2024-01-24 17:08:07 +0530

    yess

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import {useEffect,useState} from 'react'
 import './App.css';
+import axios from 'axios'
+
+ 
 
 function App() {
+ var [data,setData]=useState()
+
+ 
+
+
+  useEffect (()=>{
+    axios.get("https://fakestoreapi.com/products").then(function(res){
+        // console.log(res.data)
+          
+        setData(res?.data);
+      }
+      
+      )
+      
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {
+     
+data.map((fun)=>{
+ return(<div >{fun.id}</div>)
+})
+  }
     </div>
   );
 }
