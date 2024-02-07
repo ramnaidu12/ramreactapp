28022030e3d3eec81923020941a210b4e0e30b62
ramnaidu12 <ramnaidudoddi415@gmail.com>
2024-02-07 10:43:23 +0530

    cx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
-import './App.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import Axi from './axe';
-import Addcart from './addcart';
+
 import { Outlet } from 'react-router';
 function App() {
   return (
--- src/addcart.jsx
+++ src/addcart.jsx
@@ -2,9 +2,8 @@ import { connect } from "react-redux";
 import React from "react";
 
 
-function Addcart(props){
-console.log("pro",props)
-console.log("ppy",props.axi.obj)
+function Addcart(){
+
     return (<div className="container">
 {
     props.axi.obj.map(pros=><>
@@ -13,7 +12,7 @@ console.log("ppy",props.axi.obj)
    <img className="m-2 " width="10%"src={pros.pro.image}/>
     <h1>${pros.pro.price}</h1>
     <h6>{pros.pro.title}</h6>
-    <button className="btn btn-secondary" onClick={()=>{props.pro.dispatch({type:"check",payload:{pros} })}}>check out</button>
+    <button className="btn btn-secondary" >check out</button>
     </div>
     </>)
 }
